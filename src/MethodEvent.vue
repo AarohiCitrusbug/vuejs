3aff6d8d8450cb2522a54f620ad1b72a388afd71
AarohiCitrusbug <109519005+AarohiCitrusbug@users.noreply.github.com>
2022-10-19 19:15:02 +0530

    Methods, Event handling, Form handling, Modifiers

---
+++ src/MethodEvent.vue
@@ -0,0 +1,39 @@
+<template>
+  <h2>{{ 2 + 3 + 5 }}</h2>
+  <h2>Method Example1-{{ add(2, 3, 5) }}</h2>
+  <h2>Method Example2-{{ multiply(baseValue) }}</h2>
+  <h2>{{ name }}</h2>
+  <h2>{{ count }}</h2>
+  <div>
+    <button v-on:click="name = 'Test2'">Change Name</button>
+    <button v-on:click="increment(1)">Increment</button>
+    <button @click="decrement(1)">Decrement</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "MethodEvent",
+  data() {
+    return {
+      name: "Test1",
+      baseMultiplier: 5,
+      baseValue: 2,
+      count: 0,
+    };
+  },
+  methods: {
+    add(a, b, c) {
+      return a + b + c;
+    },
+    multiply(num) {
+      return num * this.baseMultiplier;
+    },
+    increment(num) {
+      this.count += num;
+    },
+    decrement(num) {
+      this.count -= num;
+    },
+  },
+};
+</script>
