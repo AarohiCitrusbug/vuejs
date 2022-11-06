b4cc510e156f96eb0d761022da4505b34617a552
AarohiCitrusbug <aarohi.citrusbug.com>
2022-11-07 01:02:07 +0530

    Added layouts

---
+++ src/components/Layouts/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="topnav">
+        <div>
+            <a @click="logOut" >Log Out</a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"Header",
+    data(){
+    },
+    methods:{
+        logOut:function(){
+            localStorage.removeItem('user');
+            localStorage.removeItem('firstName');
+            this.$router.push({name:'Login'})
+        }
+    }
+}
+</script>
---
+++ src/components/Layouts/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="wrapper d-flex align-items-stretch">
+		<nav id="sidebar">
+            <ul class="list-unstyled components mb-5">
+            <li class="active">
+                <a href="#">Dashboard</a>
+            </li>
+            <li>
+                <a href="#">Binding - Text, HTML, Attributes, Classes, Styles</a>
+            </li>
+            <li>
+                <a href="#">Conditional rendering, List rendering, Lists & key</a>
+            </li>
+            <li>
+                <a href="#">Methods, Event handling, Form handling, Modifiers</a>
+            </li>
+            <li>
+                <a href="#">Computed properties, Setters, Watchers</a>
+            </li>
+            <li>
+                <a href="#">Components with props, Prop types & validations</a>
+            </li>
+            <li>
+                <a href="#">Provide & inject, Events, Components & v-model</a>
+            </li>
+            <li>
+                <a href="#">Slot and styles</a>
+            </li>
+            <li>
+                <a href="#">Dynamic components, Teleport components</a>
+            </li>
+            <li>
+                <a href="#">HTTP & Vue</a>
+            </li>
+            <li>
+                <a href="#">Lifecycle Hooks, Template refs</a>
+            </li>
+            <li>
+                <a href="#">Mixins, Composition API</a>
+            </li>
+            </ul>
+    	</nav>
+		</div>
+</template>
+<script>
+export default {
+    name:"Sidebar",
+    data(){
+    }
+}
+</script>
